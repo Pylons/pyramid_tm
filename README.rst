11503584085bcdf68098e21139dc525f3a76ccf6
Rocky Burt <rocky@serverzen.com>
2011-02-21 18:03:30 -0330

    Some initial structure

---
+++ README.rst
@@ -0,0 +1,11 @@
+``pyramid_tm``
+==============
+
+``pyramid_tm`` is a package which allows Pyramid requests to join
+the active transaction as provided by the `transaction
+<http://pypi.python.org/pypi/transaction>`_ package.
+
+See `http://docs.pylonsproject.org/projects/pyramid_tm/dev/
+<http://docs.pylonsproject.org/projects/pyramid_tm/dev/>`_ 
+or ``docs/index.rst`` in this distribution for detailed
+documentation.
---
+++ setup.py
@@ -0,0 +1,55 @@
+##############################################################################
+#
+# Copyright (c) 2008-2011 Agendaless Consulting and Contributors.
+# All Rights Reserved.
+#
+# This software is subject to the provisions of the BSD-like license at
+# http://www.repoze.org/LICENSE.txt.  A copy of the license should accompany
+# this distribution.  THIS SOFTWARE IS PROVIDED "AS IS" AND ANY AND ALL
+# EXPRESS OR IMPLIED WARRANTIES ARE DISCLAIMED, INCLUDING, BUT NOT LIMITED TO,
+# THE IMPLIED WARRANTIES OF TITLE, MERCHANTABILITY, AGAINST INFRINGEMENT, AND
+# FITNESS FOR A PARTICULAR PURPOSE
+#
+##############################################################################
+
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+try:
+    README = open(os.path.join(here, 'README.rst')).read()
+    CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+except IOError:
+    README = CHANGES = ''
+
+install_requires = [
+    'pyramid>=1.0',
+    'transaction',
+    ]
+
+
+setup(name='pyramid_handlers',
+      version='0.1',
+      description='Pyramid handlers emulate Pylons 1 controllers',
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python",
+        "Framework :: Pylons",
+        "Topic :: Internet :: WWW/HTTP :: WSGI",
+        "License :: Repoze Public License",
+        ],
+      keywords='wsgi pylons pyramid transaction',
+      author="Rocky Burt",
+      author_email="pylons-devel@googlegroups.com",
+      url="http://docs.pylonsproject.org",
+      license="BSD-derived (http://www.repoze.org/LICENSE.txt)",
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=install_requires,
+      tests_require=install_requires,
+      test_suite="pyramid_tm",
+      entry_points='',
+      )
