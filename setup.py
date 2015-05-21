5e2b2bc0578f0ccee5fd9b2264174190b36c6de7
Michael Merickel <michael@merickel.org>
2015-05-20 20:25:02 -0500

    prep 0.12

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ docs_extras = [
 ]
 
 setup(name='pyramid_tm',
-      version='0.11',
+      version='0.12',
       description=('A package which allows Pyramid requests to join the '
                    'active transaction'),
       long_description=README + '\n\n' + CHANGES,
