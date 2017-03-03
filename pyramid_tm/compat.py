d82e2c58ceb2256b15bbedd713efa5f1206af1ae
Michael Merickel <michael@merickel.org>
2017-03-02 20:32:27 -0600

    update to latest version of reraise from six and switch to PY2 checks

--- pyramid_tm/compat.py
+++ pyramid_tm/compat.py
@@ -1,35 +1,47 @@
 import sys
 
-PY3 = sys.version_info[0] == 3
+PY2 = sys.version_info[0] == 2
 
-if PY3: # pragma: no cover
-    import builtins
-    exec_ = getattr(builtins, "exec")
-
-    text_type = str
-    binary_type = bytes
-
-    def reraise(tp, value, tb=None):
-        if value.__traceback__ is not tb:
-            raise value.with_traceback(tb)
-        raise value
-
-else: # pragma: no cover
-    def exec_(code, globs=None, locs=None):
-        """Execute code in a namespace."""
-        if globs is None:
-            frame = sys._getframe(1)
-            globs = frame.f_globals
-            if locs is None:
-                locs = frame.f_locals
-            del frame
-        elif locs is None:
-            locs = globs
-        exec("""exec code in globs, locs""")
-
+if PY2:
     text_type = unicode
-    binary_type = str
+
+    def exec_(_code_, _globs_=None, _locs_=None):
+        """Execute code in a namespace."""
+        if _globs_ is None:
+            frame = sys._getframe(1)
+            _globs_ = frame.f_globals
+            if _locs_ is None:
+                _locs_ = frame.f_locals
+            del frame
+        elif _locs_ is None:  # pragma: nocover
+            _locs_ = _globs_
+        exec("""exec _code_ in _globs_, _locs_""")
 
     exec_("""def reraise(tp, value, tb=None):
-    raise tp, value, tb
+    try:
+        raise tp, value, tb
+    finally:
+        tb = None
 """)
+
+else:
+    text_type = str
+
+    def reraise(tp, value, tb=None):
+        try:
+            if value is None:  # pragma: nocover
+                value = tp()
+            if value.__traceback__ is not tb:  # pragma: nocover
+                raise value.with_traceback(tb)
+            raise value
+        finally:
+            value = None
+            tb = None
+
+def text_(s):
+    if isinstance(s, bytes):
+        try:
+            return s.decode('utf-8')
+        except UnicodeDecodeError:
+            return s.decode('latin-1')
+    return text_type(s)
